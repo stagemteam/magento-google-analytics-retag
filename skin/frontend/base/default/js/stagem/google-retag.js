1ed78bea5a03cb42b780f3458ad2ebafb4d02267
popovsergiy <popow.sergiy@gmail.com>
2018-08-23 18:50:22 +0300

    Send data only when form is valid

--- skin/frontend/base/default/js/stagem/google-retag.js
+++ skin/frontend/base/default/js/stagem/google-retag.js
@@ -1,21 +1,35 @@
-var targets = Stagem.GoogleRetag.targets;
-targets.forEach(function (target) {
-  console.log('target', target);
-  var fullActions = target['full_action_name'].split(',');
-  console.log('fullActions', fullActions);
+jQuery(document).ready(function () {
+  var targets = Stagem.GoogleRetag.targets;
+  jQuery.each(targets, function (i, target) {
+    var fullActions = target['full_action_name'].split(',');
+    jQuery.each(fullActions, function (i, action) {
+      if (('*' === action) || (action === Stagem.GoogleRetag.fullActionName)) {
+        jQuery(target['selector']).on(target['event'], function () {
+          var elm = jQuery(this);
+          var form = elm.closest('form');
+          if (form.length) {
+            // Find required empty text elements
+            var emptyElms = form.find('input.required-entry:text').filter(function () {
+              return jQuery(this).is(':visible') && this.value === '';
+            });
 
-  fullActions.forEach(function (action) {
-    if (('*' === action) || (action === Stagem.GoogleRetag.fullActionName)) {
-      return;
-    }
-    jQuery(target['selector']).on(target['event'], function () {
-      var elm = jQuery(this);
-      var form = elm.closest('form');
-      if (form.length && form.has('[class^=required-], [class^=validate-]')) {
+            // Check if there are no empty elements and elements with failed validation
+            if (emptyElms.length < 1 && form.find('.validation-failed, .mage-error').length < 1) {
+              eval(target['code']);
+              console.log("Run target for form: " + target['code']);
+            }
+          } else if (typeof elm.is('button') || elm.is('[type=submit]')) {
+            if (!elm.hasClass('novalidate')) {
+              eval(target['code']);
+              console.log("Run target for button: " + target['code']);
+            }
 
+          } else {
+            eval(target['code']);
+            console.log("Run target: " + target['code']);
+          }
+        });
       }
-      eval(target['code']);
-      console.log("Run target: " + target['code']);
     });
   });
 });
